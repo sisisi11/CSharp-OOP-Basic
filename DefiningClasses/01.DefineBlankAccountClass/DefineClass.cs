4fcb5305c42c859a12330cc9cb2a36ec977e173e
Anton Stoychev <everton.sky.1231@gmail.com>
2017-06-28 10:10:57 +0300

    DefiningClasses

---
+++ DefiningClasses/01.DefineBlankAccountClass/DefineClass.cs
@@ -0,0 +1,14 @@
+﻿namespace _01.DefineBankAccountClass
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class DefineClass
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
